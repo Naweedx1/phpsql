c6c27b9f0b3353783edbdc54d1dda7c52511cbdf
Naweed <naweedx1@gmail.com>
2023-12-03 21:12:31 +0430

    update

--- controllers/note.php
+++ controllers/note.php
@@ -10,4 +10,14 @@ $id = $_GET['id'];
 
 $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
 
+if (!$note) {
+    abort();
+};
+
+$currentUserId = '1';
+
+if ($note['user-id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+};
+
 require "views/note.view.php";
